2571088b70b13584faa85cb518223b2cc3d23ecf
chrisjoubie <114072746+chrisjoubie@users.noreply.github.com>
2022-12-23 12:20:50 +0200

    Add files via upload

---
+++ Attempt1.py
@@ -0,0 +1,7 @@
+echo "# learnpy" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/chrisjoubie/learnpy.git
+git push -u origin main
